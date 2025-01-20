61603f7e98aa77f3670cfe3b9ccc54462279b4ba
ErickCastroo <castrocamachoerickmiguel21@gmail.com>
2025-01-20 16:46:48 -0700

    Refactorizar el componente de la aplicación para integrar ThemeProvider y GradientFollowMouse, mejorando la estructura de la interfaz de usuario y la compatibilidad con el modo oscuro

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,45 @@
+import React from 'react';
 import { useTranslation } from 'react-i18next'
 
-import { ThemeSwitcher } from '@/components/CambiadordeTema'
+import { ThemeProvider } from '@/context/ThemeContext';
 
-function App() {
+import { ThemeSwitcher } from '@/components/CambiadordeTema';
+import { GradientFollowMouse } from '@/components/MouseGradiante';
+import i18n from './utils/config/I18n';
 
-  const { t, i18n } = useTranslation()
+const App: React.FC = () => {
+  const { t } = useTranslation()
 
   const changeLanguage = (lng: string) => {
     i18n.changeLanguage(lng) // Cambiar idioma
   }
 
   return (
-    <div className='w-full h-screen bg-white dark:bg-gray-900 text-black dark:text-white'>
-      <h1 className='text-lg mb-6'>{t('welcome')}</h1>
-      <p className='text-lg mb-6'>{t('description')}</p>
-      <div className='space-x-4'>
-        <button
-          onClick={() => changeLanguage('en')}
-          className='px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 focus:outline-none'
-        >
-          English
-        </button>
-        <button
-          onClick={() => changeLanguage('es')}
-          className='px-4 py-2 bg-green-500 text-white rounded hover:bg-green-600 focus:outline-none'
-        >
-          Español
-        </button>
-        <ThemeSwitcher />
-      </div>
-    </div>
-  )
-}
+    <ThemeProvider>
+      <GradientFollowMouse>
+        <div className='flex justify-center items-center min-h-screen relative z-20'>
+          <h1 className='text-4xl font-bold text-center text-white'>{t('welcome')}</h1>
+          <div className="space-x-4">
+            <button
+              onClick={() => changeLanguage('en')}
+              className="px-6 py-3 bg-blue-500 text-white rounded-lg hover:bg-blue-600 transition duration-300"
+            >
+              English
+            </button>
+            <button
+              onClick={() => changeLanguage('es')}
+              className="px-6 py-3 bg-green-500 text-white rounded-lg hover:bg-green-600 transition duration-300"
+            >
+              Español
+            </button>
+            </div>
+          </div>
+          <div className='absolute right-5 top-5'>
+            <ThemeSwitcher />
+          </div>
+      </GradientFollowMouse>
+    </ThemeProvider>
+  );
+};
 
-export { App }
+export { App };
