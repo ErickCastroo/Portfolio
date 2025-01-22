4027ffef1b08c664119244e53bb185ebabf25630
ErickCastroo <castrocamachoerickmiguel21@gmail.com>
2025-01-21 17:39:09 -0700

    Mejorar el estilo del botón de cambio de tema, corregir enlace de navegación en el componente Header y agregar estilos personalizados para el scrollbar en index.css

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -30,7 +30,7 @@ function Header() {
             </a>
           </li>
           <li>
-            <a className='group flex items-center py-3 active' href='#Experiencia'>
+            <a className='group flex items-center py-3 active' href='#experiencia'>
               <span
                 className='nav-indicator mr-4 h-px w-8 bg-slate-600 transition-all group-hover:w-16 dark:group-hover:bg-slate-200 group-hover:bg-slate-50 group-focus-visible:w-16 dark:group-focus-visible:bg-slate-200 group-focus-visible:bg-slate-900 motion-reduce:transition-none'></span>
               <span className='nav-text text-xs font-bold uppercase tracking-widest text-slate-500 dark:group-hover:text-slate-200 dark:group-hover:bg-slate-900 group-hover:bg-slate-50 dark:group-focus-visible:text-slate-200 group-focus-visible:bg-slate-900'>{t('Experience')}</span>
